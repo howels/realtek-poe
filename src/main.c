4f83fc201a7a8ad34de5e23fba9ae8e5860592cf
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2024-04-20 19:14:50 -0500

    [WIP]: dialect_rtl: add RTL8328B (or 8238B ?) compatible dialect

--- src/main.c
+++ src/main.c
@@ -44,7 +44,7 @@ struct poe_ctx {
 
 static struct mcu main_mcu;
 static struct blob_buf b;
-static const struct poe_dialect *dialect = &broadcom_dialect;
+static const struct poe_dialect *dialect = &realtek_dialect;
 
 static struct poe_ctx *ubus_to_poe_ctx(struct ubus_context *u)
 {
